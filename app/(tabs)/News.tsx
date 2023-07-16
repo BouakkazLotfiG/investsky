147436854db4ea8e50bceb2f6c04013aeb354e3b
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-07-16 22:34:50 +0100

    News screen, displaying added stock

--- app/(tabs)/News.tsx
+++ app/(tabs)/News.tsx
@@ -1,10 +1,54 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../store';
+import { SIZES, COLORS, FONTS } from '../../constants/Theme';
+import StockCard from '../../components/StockCard';
 
 export default function News() {
+  const stock = useSelector((state: RootState) => state.user);
   return (
-    <View>
-      <Text>News</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          {stock.length === 0 ? (
+            <Text style={styles.header}>No stock added</Text>
+          ) : (
+            <>
+              <Text style={styles.header}>Added Stock</Text>
+
+              <View style={styles.cardsContainer}>
+                {stock.map((item) => (
+                  <StockCard key={item.symbol} stock={item} />
+                ))}
+              </View>
+            </>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: SIZES.padding * 2,
+    flexGrow: 1,
+    gap: 15,
+    backgroundColor: COLORS.white,
+    width: SIZES.width,
+  },
+  header: {
+    ...FONTS.h1,
+    color: COLORS.black,
+    paddingVertical: SIZES.paddingVertical,
+    paddingHorizontal: SIZES.paddingHorizontal * 1.5,
+  },
+  cardsContainer: {
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    gap: 15,
+    paddingHorizontal: SIZES.paddingHorizontal * 1.5,
+    paddingVertical: SIZES.paddingVertical,
+  },
+});
